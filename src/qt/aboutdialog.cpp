e0bd0cc3e4062f20c93516f5abb1dbfdc7b64589
lacoin2020 <lacoin.2020@protonmail.com>
2020-06-29 02:32:28 +0200

    v0.1

---
+++ src/qt/aboutdialog.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2011-2020 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "aboutdialog.h"
+#include "ui_aboutdialog.h"
+
+#include "clientmodel.h"
+#include "clientversion.h"
+
+// Copyright year (2009-this)
+// Todo: update this when changing our copyright comments in the source
+const int ABOUTDIALOG_COPYRIGHT_YEAR = 2020;
+
+AboutDialog::AboutDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AboutDialog)
+{
+    ui->setupUi(this);
+
+    // Set current copyright year
+    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009 - %1 ").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + tr("The Bitcoin developers") + QString("<br>") + tr("Copyright") + QString(" &copy; 2011 - %1 ").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + tr("The Litecoin developers") + QString("<br>") + tr("Copyright") + QString(" &copy; 2020 - %1 ").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + tr("The Lacoin developers"));
+
+
+}
+
+void AboutDialog::setModel(ClientModel *model)
+{
+    if(model)
+    {
+        ui->versionLabel->setText(model->formatFullVersion());
+    }
+}
+
+AboutDialog::~AboutDialog()
+{
+    delete ui;
+}
+
+void AboutDialog::on_buttonBox_accepted()
+{
+    close();
+}
